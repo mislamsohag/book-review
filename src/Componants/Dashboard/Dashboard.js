260ed8ecb36c20751b70066716945220bab79aee
soahg <mislamsohag@gmail.com>
2022-04-04 17:19:37 +0600

    Rechat Add

--- src/Componants/Dashboard/Dashboard.js
+++ src/Componants/Dashboard/Dashboard.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import { useEffect, useState } from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+import Rechat from '../Rechat/Rechat';
+
+
 const Dashboard = () => {
+    const [chatData, setChatData] = useState([]);
+
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setChatData(data))
+    }, [])
+    // console.log(chatData)
+
     return (
         <div>
-            <p>This is Chart</p>
-        </div>
+            <LineChart width={800} height={400} data={chatData}>
+                <Line type="monotone" dataKey={'sell'} stroke="#8884d8" />
+                <Line type="monotone" dataKey={'investment'} stroke="#8884d8" />
+                <Line type="monotone" dataKey={'revenue'} stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey={'month'} />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+
+        </div >
     );
 };
 
