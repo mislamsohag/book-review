ab2ab1ee9db54fb6b62e45506e9b823287c27841
soahg <mislamsohag@gmail.com>
2022-04-05 23:12:42 +0600

    Address Add

--- src/Componants/About/About.js
+++ src/Componants/About/About.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <h1>This is About Page</h1>
+    <div className='border p-8 bg-blue-100 w-full'>
+      <div className='flex justify-between items-center'>
+        <div>
+          <h1>Village: Vhuighor.</h1>
+          <h1>City: Narayanganj</h1>
+          <h1>Phone: +8801812060163</h1>
+          <h1>Email: mislamsohag@gmail.com</h1>
+        </div>
+        <div>
+          <h1 className='text-2xl'>BANGLADESH</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
