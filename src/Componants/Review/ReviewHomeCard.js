8351349052bd7e9a04a5cc756f749cfa4110bfbc
soahg <mislamsohag@gmail.com>
2022-04-05 22:58:02 +0600

    Some Add

--- src/Componants/Review/ReviewHomeCard.js
+++ src/Componants/Review/ReviewHomeCard.js
@@ -17,7 +17,7 @@ const ReviewHomeCard = ({ review }) => {
                     <div class="px-6 pt-4 pb-2">
                         <span>
                             <Link to='/reviews'
-                                className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
+                                className='inline-block bg-sky-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
                             >
                                 See All Reviews
                             </Link>
